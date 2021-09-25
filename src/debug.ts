144376e977d021103aba637b5c377616dedb1b03
memanot <menemty@gmail.com>
2021-09-25 20:55:48 +0300

    debug default

--- src/debug.ts
+++ src/debug.ts
@@ -1,5 +1,20 @@
-const debug = async () => {
-    // 
+import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+type TAppSettings = {}
+
+class App {
+  static version = 1
+  static env = process.env
+  static rootPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '..')
+
+  constructor({}: TAppSettings) {
+    dotenv.config({ path: path.join(App.rootPath, `.env`) })
+  }
+
+  async start() {
+  }
 }
 
-debug()
+new App({}).start()
